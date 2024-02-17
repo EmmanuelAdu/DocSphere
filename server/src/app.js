3e89fe56ffb1caebd6949dec1a6e8044913995fe
EmmanuelAdu <christorenzo20@gmail.com>
2024-02-17 13:59:53 +0000

    SetUp of the express.js server

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+console.log('Hello World')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/status', (req, res) => {
+    res.send("{message: 'Hello World'}")
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API')
+})
+
+app.listen(process.env.PORT || 8081)
