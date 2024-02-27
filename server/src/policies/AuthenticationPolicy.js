9f16f019f060118920453946de855d7a9e01427e
EmmanuelAdu <christorenzo20@gmail.com>
2024-02-27 14:30:15 +0000

    Implemented Regex to handle password authentication

--- server/src/policies/AuthenticationPolicy.js
+++ server/src/policies/AuthenticationPolicy.js
@@ -5,11 +5,13 @@ const Joi = require('joi')
 
 // Define validation schema for email and password
 const emailSchema = Joi.string().email().required()
-const passwordSchema = Joi.string().min(6).required()
+const passwordSchema = Joi.string().pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$')).required().messages({
+  'string.pattern.base': 'Password must contain at least one letter and one number and be at least 6 characters long' // Custom error message for pattern validation
+})
 
 // Define custom error messages
 const emailErrorMessage = 'Please enter a valid email address'
-const passwordErrorMessage = 'Password must be at least 6 characters long'
+const passwordErrorMessage = 'Password must contain at least one letter and one number and be at least 6 characters long'
 
 module.exports = {
   register(req, res, next) {
@@ -17,7 +19,7 @@ module.exports = {
     const schema = Joi.object({
       email: emailSchema,
       password: passwordSchema
-    })
+    });
     
     const { error } = schema.validate(req.body, { abortEarly: false }) // Abort early to show all errors
     if (error) {
@@ -40,3 +42,4 @@ module.exports = {
     next()
   }
 }
+
